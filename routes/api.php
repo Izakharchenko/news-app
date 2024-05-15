07ea9ad81f3e58442edfdbbf227e360c6809c981
Ivan Zakharchenko <ipzakharchenko@gamil.com>
2024-05-15 19:39:45 +0300

    without wrapping data

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/categories/{category}/news', [CategoryController::class, 'news']);
+Route::get('/categories/{category}/news', [CategoryController::class, 'news'])->name('categories.news');
 Route::apiResources([
     'categories' => CategoryController::class,
     'news' => NewsController::class
